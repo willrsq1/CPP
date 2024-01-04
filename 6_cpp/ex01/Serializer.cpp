69105f876f5d5d4615b8fb5e97b325cec34b36bb
Will <william.ruetsuquet@gmail.com>
2024-01-04 07:27:19 +0100

    idk

--- 6_cpp/ex01/Serializer.cpp
+++ 6_cpp/ex01/Serializer.cpp
@@ -28,11 +28,9 @@ Serializer& Serializer::operator= (const Serializer& other)
 uintptr_t Serializer::serialize(Data* ptr)
 {
 	return reinterpret_cast<uintptr_t>(ptr);
-	// return (uintptr_t)ptr;
 }
 
 Data* Serializer::deserialize(uintptr_t raw)
 {
 	return reinterpret_cast<Data *>(raw);
-	// return (Data *)raw;
 }
